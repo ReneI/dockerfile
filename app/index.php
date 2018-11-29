00eaf5f715d8173ab4f40285dc319686a0391a1a
Rene <reidisy@gmail.com>
2018-11-28 18:35:07 -0600

    imagenes

--- app/index.php
+++ app/index.php
@@ -31,7 +31,7 @@
   
               <?php
 
-$conexion=mysqli_connect("localhost", "root", "test", "directorio");
+$conexion=mysqli_connect("localhost", "admin", "test", "directorio");
     $consulta="SELECT * FROM directorio";
     $query=mysqli_query($conexion, $consulta);
     ?>
